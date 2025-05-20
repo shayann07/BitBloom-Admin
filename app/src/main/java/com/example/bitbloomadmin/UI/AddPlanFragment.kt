e02d502ebf5841bdfb0ee93714c2aa98534f7fc6
Aqeelabbas34 <aqeelabbas34@gmail.com>
2025-05-20 20:23:36 +0500

    plans

--- app/src/main/java/com/example/bitbloomadmin/UI/AddPlanFragment.kt
+++ app/src/main/java/com/example/bitbloomadmin/UI/AddPlanFragment.kt
@@ -5,9 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.example.bitbloomadmin.R
-import com.google.android.material.button.MaterialButton
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [PlanFragment.newInstance] factory method to
+ * Use the [AddPlanFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PlanFragment : Fragment() {
+class AddPlanFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,15 +35,26 @@ class PlanFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plan, container, false)
+        return inflater.inflate(R.layout.fragment_add_plan, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val addPlanButton = view.findViewById<MaterialButton>(R.id.btnAddPlan)
-        addPlanButton.setOnClickListener {
-            findNavController().navigate(R.id.action_planFragment_to_addPlanFragment)
-        }
-
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment AddPlanFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            AddPlanFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
     }
 }
--- app/src/main/java/com/example/bitbloomadmin/Viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/bitbloomadmin/Viewmodel/UserViewModel.kt
@@ -1,16 +1,9 @@
 package com.example.bitbloomadmin.Viewmodel
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.bitbloomadmin.Repository.UserRepository
-import com.example.bitbloomadmin.models.AccountModel
 import com.example.bitbloomadmin.models.AnnouncementModel
-import com.example.bitbloomadmin.models.UserModel
 import com.example.bitbloomadmin.models.UserWithAccount
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
